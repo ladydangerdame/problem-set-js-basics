d390510d78dff87ce291a8d55af2f04a5c27033b
ladydangerdame <turbofemme@gmail.com>
2016-10-30 00:20:02 -0700

    Completed with callback function that works better than solution booya

--- challenges/primes.js
+++ challenges/primes.js
@@ -10,3 +10,22 @@
 */
 
 // YOUR CODE HERE
+function isPrime(num) {
+  for (var i=2; i<num; i++) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function prime(max) {
+  var primesArray = [];
+  for (var i=2; i<=max; i++) {
+    if (isPrime(i)) {
+    primesArray.push(i);
+      }
+  }
+return primesArray;
+  }
+  
